d9e46b3b03aeba20b6664173378a73699c565974
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-08-19 23:14:29 +0530

    Add files via upload

---
+++ print_vardiac_function.cpp
@@ -0,0 +1,47 @@
+#include "./print_stl_operator_overloaded.cpp"
+
+
+// base case
+void _w_recursive()
+{
+}
+
+ 
+// Variadic function Template that takes
+// variable number of arguments and prints
+// all of them space separated recursively.
+template <typename T, typename... Types>
+void _w_recursive(T var1, Types... var2)
+{
+    cout  << var1 << " ";
+ 
+    _w_recursive(var2...);
+}
+
+// Variadic function Template that takes
+// variable number of arguments and prints
+// all of them space separated recursively _with spacing maintained.
+template <typename... Types>
+void _w(Types... var2)
+{
+    cout << spacing  ;
+ 
+    _w_recursive(var2...);
+}
+
+// base case
+void _w_()
+{
+}
+
+template <typename... Types>
+void _w_(Types... var2)
+{
+    cout << spacing  ;
+ 
+    _w_(var2...);
+}
+
+// int main(){
+//     _w({"www","refsfd","234w3"},true,233,24214124214,23.23412);
+// }
---
+++ recursion_utilities.cpp
@@ -0,0 +1,45 @@
+#include "./print_vardiac_function.cpp"
+#include "./depricated.cpp"
+
+string unitSeperator = "|     ";
+string enterArrow = "|-----";
+
+// recurse enter _e()
+template <typename T, typename... Types>
+void _e(T functionName, Types... t)
+{
+    string fnName = (string)functionName+"() ";
+    string enterSpace=spacing;
+
+    if(enterSpace.size()>=unitSeperator.size()){ enterSpace = enterSpace.erase(enterSpace.size() - unitSeperator.size());
+    enterSpace+=enterArrow;}
+
+    cout<<enterSpace<<fnName;
+    spacing += unitSeperator;
+    _w(t...);
+}
+void _e()
+{
+    spacing += unitSeperator;
+    cout << spacing;
+}
+
+
+//b(): recurse exit/base case ; _e(args): recurse enter; _w(): print anything with current spacing
+template <typename... Types>
+void _b(Types... t)
+{
+    spacing = spacing.erase(spacing.size() - unitSeperator.size());
+    _w(dep("return "), t...);
+}
+//b(): recurse exit/base case ; _e(args): recurse enter; _w(): print anything with current spacing
+void _b()
+{
+    spacing = spacing.erase(spacing.size() - unitSeperator.size());
+    _w(dep("return"));
+}
+
+//b(): recurse exit/base case ; _e(args): recurse enter; _w(): print anything with current spacing
+#define _e(...) _e( __FUNCTION__ __VA_OPT__(,) __VA_ARGS__)
+#define recurse _e
+#define returnRecurse _b
